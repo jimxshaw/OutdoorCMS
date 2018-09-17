26564d6c9a6eb7009281c89cb64193acce65dfb5
Jim Shaw <jimxshaw@gmail.com>
2018-09-17 16:45:59 -0500

    Add routes. Add demo controller and action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # root route
+  root 'demo#index'
+
+  # simple route
+  get 'demo/index'
+
+  # default route (may go away in future versions of Rails)
+  # get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
